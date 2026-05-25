a75cd367a96079d6468e3bf5edaedb5bd86efd21
Jhon-Allan-V <jhon.veliz.h@mail.pucv.cl>
2026-05-24 23:22:08 -0400

    version 3: se agregan funciones, dfs, bfs, best_first

--- tarea3.c
+++ tarea3.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tdas/list.h"
+#include "tdas/stack.h"
+#include "tdas/queue.h"
 #include "tdas/heap.h"
 #include "tdas/extra.h"
 #include <string.h>
 #include <time.h>
 
-#define FILA x
-#define COL y
-#define OBSTACULO 0
-#define CAMINO 1
+//#define FILA x
+//#define COL y
+//#define OBSTACULO 0
+//#define CAMINO 1
 
 // Definición de la estructura para el estado del puzzle
 typedef struct {
@@ -24,6 +26,22 @@ typedef struct { int accionX, accionY; } accion;
 
 int distancia_L1(State* state) {
     return abs(state->x - (N-1)) + abs(state->y - (N-1));
+}
+
+int esSolucionFinal(State *actual){
+    return actual -> x == N-1 && actual -> y == N-1;
+}
+
+int pilaEstaVacia(Stack *pila){
+    return stack_top(pila) == NULL;
+}
+
+int colaEstaVacia(Queue *cola){
+    return queue_front(cola) == NULL;
+}
+
+int heapEstaVacia(Heap *heap){
+    return heap_top(heap) == NULL;
 }
 
 // Función para imprimir el estado actual
@@ -42,6 +60,24 @@ void imprimirEstado(const State *estado) {
     }
 }
 
+void imprimirCamino(State *camino){
+    accion *movimiento = list_first(camino -> actions);
+      /*{-1,0},//arriba
+        {1,0},//abajo
+        {0,-1},//izq
+        {0,1}//der*/
+    printf("\nPasos totales a realizar -> %i\n", camino -> steps);
+
+    while (movimiento != NULL){
+        if (movimiento -> accionX == -1 && movimiento -> accionY == 0)printf("Arriba\n");
+        if (movimiento -> accionX == 1 && movimiento -> accionY == 0)printf("Abajo\n");
+        if (movimiento -> accionX == 0 && movimiento -> accionY == -1)printf("Izquierda\n");
+        if (movimiento -> accionX == 0 && movimiento -> accionY == 1)printf("Derecha\n");
+
+        movimiento = list_next(camino -> actions);
+    }
+}
+
 State crearEstadoInicial(int maze[N][N], int dificultad){
     State estado;
      // Copiar el laberinto generado al estado
@@ -55,13 +91,34 @@ State crearEstadoInicial(int maze[N][N], int dificultad){
 
 State *transition(State *actual, accion movimiento){
     State *nuevo = malloc(sizeof(State));
-    if (!nuevo) exit(EXIT_FAILURE);
+    if (nuevo == NULL) exit(EXIT_FAILURE);
 
     *nuevo = *actual;
 
-    nuevo -> FILA += movimiento.accionX;
-    nuevo -> COL += movimiento.accionY;
+    nuevo -> x += movimiento.accionX;
+    nuevo -> y += movimiento.accionY;
     nuevo -> steps += 1;//actual -> steps +1;
+    nuevo -> actions = list_create();
+
+    accion *aux = list_first(actual -> actions);
+
+    while (aux != NULL){
+        accion *copia = malloc(sizeof(accion));
+        if (copia == NULL) exit(EXIT_FAILURE);
+
+        *copia = *aux;
+
+        list_pushBack(nuevo -> actions, copia);
+
+        aux = list_next(actual -> actions);
+    }
+
+    accion *nuevaAccion = malloc(sizeof(accion));
+    if (nuevaAccion == NULL) exit(EXIT_FAILURE);
+
+    *nuevaAccion = movimiento;
+
+    list_pushBack(nuevo -> actions, nuevaAccion);
 
     return nuevo;
 }
@@ -79,38 +136,131 @@ List *getAdjNodes(State *actual){
     for (unsigned short i = 0; i < 4; i++){
         accion movimiento = acciones[i];
 
-        int nuevaFila = actual -> FILA + movimiento.accionX;
-        int nuevaCol = actual -> COL + movimiento.accionY;
+        int nuevaFila = actual -> x + movimiento.accionX;
+        int nuevaCol = actual -> y + movimiento.accionY;
 
         //verificar si se sale de los limites
         if (nuevaFila < 0 || nuevaFila >= N ||
             nuevaCol < 0 || nuevaCol >= N) continue;
 
         //verificar si estamos en un obstaculo
-        if (actual -> maze[nuevaFila][nuevaCol] == OBSTACULO) continue;
+        if (actual -> maze[nuevaFila][nuevaCol] == 1) continue;
 
         State *nuevo = transition(actual, movimiento);
         list_pushBack(vecinos, nuevo);
     }
     return vecinos;
 }
-/*
-void dfs(const State *estado_inicial){
-    State *copiaEstado = malloc(sizeof(State));
-    if (!copiaEstado) exit(EXIT_FAILURE);
 
-    copiaEstado = estado_inicial;
-    copiaEstado -> actions = list_create();
+void dfs(State *estado_inicial){
 
-    Stack *pila = stack_create();
+    Stack *pila = stack_create(NULL);
+
+    int visitado[N][N] = {0};
+
     stack_push(pila, estado_inicial);
 
-    while (!isEmpty(pila)){
+    while (!pilaEstaVacia(pila)){
 
+        State *actual = stack_top(pila);
+        stack_pop(pila);
+
+        if (visitado[actual -> x][actual -> y]) continue;
+
+        visitado[actual -> x][actual -> y] = 1;
+
+        if (esSolucionFinal(actual)){
+            printf("\nCamino encontrado B)\n");
+            imprimirCamino(actual);
+            return;
+        }
+
+        List *vecinos = getAdjNodes(actual);
+
+        State *vecino = list_first(vecinos);
+
+        while (vecino != NULL){
+            stack_push(pila, vecino);
+            vecino = list_next(vecinos);
+        }
     }
+    printf("\nCamino NO encontrado B(\n");
+}
 
+void bfs(State *estado_inicial){
+    Queue *cola = queue_create(NULL);
 
-}*/
+    int visitado[N][N] = {0};
+
+    queue_insert(cola, estado_inicial);
+
+    while (!colaEstaVacia(cola)){
+
+        State *actual = queue_front(cola);
+        queue_remove(cola);
+
+        if (visitado[actual -> x][actual -> y]) continue;
+
+        visitado[actual -> x][actual -> y] = 1;
+
+        if (esSolucionFinal(actual)){
+            printf("\nCamino encontrado B)\n");
+            imprimirCamino(actual);
+            return;
+        }
+
+        List *vecinos = getAdjNodes(actual);
+
+        State *vecino = list_first(vecinos);
+
+        while (vecino != NULL){
+            queue_insert(cola, vecino);
+            vecino = list_next(vecinos);
+        }
+    }
+    printf("\nCamino NO encontrado B(\n");
+}
+
+void best_first(State *estado_inicial){
+
+    Heap* heap = heap_create();
+
+    int visitado[N][N] = {0};
+
+    //heap_push(Heap* pq, void* data, int priority);
+    heap_push(heap, estado_inicial, -1 * distancia_L1(estado_inicial));
+
+    while (!heapEstaVacia(heap)){
+
+        State *actual = heap_top(heap);
+        heap_pop(heap);
+
+        if (visitado[actual -> x][actual -> y]) continue;
+
+        visitado[actual -> x][actual -> y] = 1;
+
+        if (esSolucionFinal(actual)){
+            printf("\nCamino encontrado B)\n");
+            imprimirCamino(actual);
+            return;
+        }
+        //obtener soluciones
+        List *vecinos = getAdjNodes(actual);
+
+        State *vecino = list_first(vecinos);
+
+        //recorrer estados de la lista vecinos
+        while (vecino != NULL){
+
+            int prioridad = -1 * distancia_L1(vecino);
+
+            heap_push(heap, vecino, prioridad);
+
+            vecino = list_next(vecinos);
+        }
+    }
+    printf("\nCamino NO encontrado B(\n");
+}
 
 int main() {
     // Inicializar la semilla de aleatoriedad
@@ -133,8 +283,8 @@ int main() {
     State estado_inicial = crearEstadoInicial(maze, dificultad);
 
     // Imprime el estado inicial
-    //printf("\nEstado inicial del puzzle:\n");
-    //imprimirEstado(&estado_inicial);
+    printf("\nEstado inicial del puzzle:\n");
+    imprimirEstado(&estado_inicial);
 
     printf("Distancia L1: %d\n", distancia_L1(&estado_inicial));
 /*
@@ -160,8 +310,9 @@ int main() {
 */
     char opcion;
     do {
-        printf("\npuzzle:\n");
-        imprimirEstado(&estado_inicial);
+        //printf("\npuzzle:\n");
+        //imprimirEstado(&estado_inicial);
+
         printf("\n\n***** EJEMPLO MENU ******\n");
         puts("========================================");
         puts("     Escoge método de búsqueda");
@@ -177,17 +328,14 @@ int main() {
 
         switch (opcion) {
         case '1':
-            printf("\nbuscar mejor camino con tda cola\n");
-          //dfs(&estado_inicial);
-          break;
+            dfs(&estado_inicial);
+            break;
         case '2':
-            printf("\nbuscar mejor camino con tda fila\n");
-          //bfs(estado_inicial);
-          break;
+            bfs(&estado_inicial);
+            break;
         case '3':
-            printf("\nbuscar mejor camino con tda talento al fallo\n");
-          //best_first(estado_inicial);
-          break;
+            best_first(&estado_inicial);
+            break;
         }
 
         // Evitamos pausar y limpiar pantalla si el usuario eligió salir
